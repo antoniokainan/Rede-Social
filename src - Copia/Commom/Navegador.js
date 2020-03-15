4dcb3caf85a67b10f85e21b5c0a6fc948a232565
Antonio Kainan <antoniokainan@gmail.com>
2020-03-15 14:31:36 -0300

    Create Navegador.js

---
+++ src - Copia/Commom/Navegador.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Navegador.css';
+import Logo from './logo.png';
+
+class Navegador extends Component {
+  render() { 
+      return (
+          <nav className="navegador">
+              <span><img src={Logo} width="20px" height="20px"/></span>
+              <strong><a id="link">Linha do Tempo</a></strong>
+              <strong><a id="link">Perfil</a></strong>
+              <a id="usuario">Bem-Vindo, Antonio</a>
+          </nav>
+      );
+  }
+}
+
+export default Navegador;
